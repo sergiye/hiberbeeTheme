7e4fa84ba6a08fdb056484bf5403eb2ce80ee125
Sergiy Egoshyn <egoshin.sergey@gmail.com>
2023-12-09 20:04:56 +0200

    first version

---
+++ HiberbeeTheme/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Hiberbee Theme")]
+[assembly: AssemblyDescription("Hiberbee Theme for Visual Studio")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Sergiy Egoshyn")]
+[assembly: AssemblyProduct("Hiberbee Theme")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("1.0.*")]
