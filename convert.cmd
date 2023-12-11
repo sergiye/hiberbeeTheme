b4c65dfe00d9eed3e17b5d565ecd1a65c7825df2
Sergiy Egoshyn <egoshin.sergey@gmail.com>
2023-12-11 22:15:41 +0200

    add ThemeConverter

---
+++ convert.cmd
@@ -0,0 +1,3 @@
+cd ThemeConverter
+ThemeConverter.exe -i ..\HiberbeeTheme\Hiberbee.json -o ..\HiberbeeTheme\Properties\
+ThemeConverter.exe -i ..\HiberbeeTheme\HiberbeeLight.json -o ..\HiberbeeTheme\Properties\
