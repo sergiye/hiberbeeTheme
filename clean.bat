ce15b67684950f91c00ca71b36e739354b9bd872
Sergiy Egoshyn <egoshin.sergey@gmail.com>
2023-12-24 23:45:17 +0200

    clean script added

---
+++ clean.bat
@@ -0,0 +1,10 @@
+@echo off
+
+rmdir /s /q .vs
+rmdir /s /q HiberbeeTheme\.vs
+rmdir /s /q .idea
+
+rmdir /s /q HiberbeeTheme\bin
+rmdir /s /q HiberbeeTheme\obj
+
+del ThemeConverter\MissingVSTokens.json
